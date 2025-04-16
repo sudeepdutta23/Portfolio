917fff3f179b31ef2f2a970446e437e2a09519ee
sudeep.dutta <duttasudeep105@gmail.com>
2025-04-17 01:59:12 +0530

    fix

--- src/components/GlobalThemeProvider.tsx
+++ src/components/GlobalThemeProvider.tsx
@@ -1,14 +1,19 @@
 // import { ThemeProvider } from 'next-themes'
 "use client";
-import React, { createContext, useState } from "react";
+import React, { createContext, ReactNode, useState } from "react";
 
 export const THEMES = {
   DARK: 'dark',
   LIGHT: 'light',
 }
 
+interface ThemeProvider {
+  children?: ReactNode; // '?' makes it optional
+  // Other props for your component
+}
+
 export const ThemeContext = createContext({});
-export const GlobalThemeProvider = ({ children }) => {
+export const GlobalThemeProvider = ({ children }: ThemeProvider) => {
   const [theme, setTheme] = useState(THEMES.LIGHT);
 
   return (
