0b814676a550dd9d5af91c39d37d57897d555ccf
sudeep.dutta <duttasudeep105@gmail.com>
2025-04-18 22:25:28 +0530

    fixed type issue

--- src/components/GlobalThemeProvider.tsx
+++ src/components/GlobalThemeProvider.tsx
@@ -1,23 +1,60 @@
 // import { ThemeProvider } from 'next-themes'
 "use client";
-import React, { createContext, ReactNode, useState } from "react";
+import React, {
+  createContext,
+  ReactNode,
+  useReducer,
+} from "react";
 
 export const THEMES = {
-  DARK: 'dark',
-  LIGHT: 'light',
+  DARK: "dark",
+  LIGHT: "light",
+};
+
+export const TOGGLE_THEME = "TOGGLE_THEME";
+type Theme = "light" | "dark";
+
+interface ThemeState {
+  theme: Theme;
 }
 
 interface ThemeProvider {
-  children?: ReactNode; // '?' makes it optional
-  // Other props for your component
+  children?: ReactNode;
 }
 
-export const ThemeContext = createContext({});
+interface ToggleThemeAction {
+  type: typeof TOGGLE_THEME;
+}
+
+type ThemeAction = ToggleThemeAction;
+interface ThemeContextType {
+  theme: Theme
+  setTheme: React.Dispatch<ThemeAction>;
+}
+
+const defaultThemeContext: ThemeContextType = {
+  theme: "light",
+  setTheme: () => {},
+};
+
+const themeReducer = (state: ThemeState, action: ThemeAction): ThemeState  => {
+  switch (action.type) {
+    case TOGGLE_THEME:
+      return {
+        ...state,
+        theme: state.theme === THEMES.LIGHT ? THEMES.DARK as Theme : THEMES.LIGHT as Theme,
+      };
+    default:
+      return state;
+  }
+};
+
+export const ThemeContext = createContext(defaultThemeContext);
 export const GlobalThemeProvider = ({ children }: ThemeProvider) => {
-  const [theme, setTheme] = useState(THEMES.LIGHT);
+  const [state, setTheme] = useReducer(themeReducer,  { theme: "light" });
 
   return (
-    <ThemeContext.Provider value={{ theme, setTheme }}>
+    <ThemeContext.Provider value={{ theme: state.theme , setTheme }}>
       {children}
     </ThemeContext.Provider>
   );
